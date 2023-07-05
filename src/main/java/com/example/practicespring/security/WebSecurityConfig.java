f39f7da40f0ff5f3af9f7f9132d97dd97f6be749
MabundaDee <shaydmitry232@gmail.com>
2023-07-05 16:32:05 +0200

    added security config

--- src/main/java/com/example/practicespring/security/WebSecurityConfig.java
+++ src/main/java/com/example/practicespring/security/WebSecurityConfig.java
@@ -6,6 +6,11 @@ import com.example.practicespring.security.service.UserDetailsImpl;
 import com.example.practicespring.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class WebSecurityConfig {
     @Autowired
@@ -13,8 +18,27 @@ public class WebSecurityConfig {
     @Autowired
     EntryPointJwt unauthorizedHandler;
     @Bean
-    public AuthFilter autheticationJwtTokenFilter(){
+    public AuthFilter authenticationJwtTokenFilter(){
         return new AuthFilter();
     }
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//
+//        return authProvider;
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+//        return authConfig.getAuthenticationManager();
+//    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+
 
 }
