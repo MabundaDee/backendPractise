021e461f74ea879c2bf220463d1e7909fec67e4c
MabundaDee <shaydmitry232@gmail.com>
2023-08-08 16:06:03 +0200

    security config

--- src/main/java/com/example/practicespring/security/WebSecurityConfig.java
+++ src/main/java/com/example/practicespring/security/WebSecurityConfig.java
@@ -4,35 +4,40 @@ import com.example.practicespring.security.jwt.AuthFilter;
 import com.example.practicespring.security.jwt.EntryPointJwt;
 import com.example.practicespring.security.service.UserDetailsImpl;
 import com.example.practicespring.security.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
-    @Autowired
-    UserDetailsServiceImpl userDetailsService;
-    @Autowired
+
+   private UserDetailsServiceImpl userDetailsService;
+    private PasswordEncoder passwordEncoder;
     EntryPointJwt unauthorizedHandler;
     @Bean
     public AuthFilter authenticationJwtTokenFilter(){
         return new AuthFilter();
     }
-    @Bean
+
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
-        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setUserDetailsService((UserDetailsService) userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
     }
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
+ @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
